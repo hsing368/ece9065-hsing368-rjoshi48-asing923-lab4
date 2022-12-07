2a12f41e2909bfad46bc38a2edd50da50faa3cfc
hsing368 <hsing368@uwo.ca>
2022-12-07 12:50:54 -0500

    Add backend APIs to retrieve song info

--- src/server/controllers/song.controller.js
+++ src/server/controllers/song.controller.js
@@ -1,14 +1,44 @@
 const express = require('express');
 var fs = require("fs");
 var tracks = JSON.parse(fs.readFileSync("./data/" + "raw_tracks.json", 'utf8'));
+const SongDB = require('../models/song.model');
 
+// Create a new song based on data passed in the body
+exports.song_create = function (req, res, next) {
+    // Create song object
+    let curr_song = new SongDB(
+        {
+            Title: req.body.Title,
+            Artist: req.body.Artist,
+            Album: req.body.Album,
+            Track: req.body.Track,
+            Year: req.body.Year,
+            Length: req.body.Length,
+            Genre: req.body.Genre,
+            Reviews: [],
+            Hidden: req.body.Hidden
+        }
+    );
 
+    // Write the song to the db
+    curr_song.write_to_db(function (error_msg, curr_song) {
+        if (error_msg) {
+            return next(error_msg);
+        }
+        if (req.body.desc != undefined) {
+            review_controller.addReview(req, res, next, curr_song._id);
+        }
+        res.status(200).send(curr_song);
+    }
+    );
+};
 
-exports.search_songs = function (req, resp){
+// Search song based on track name, album name, artist name and track genres
+exports.search_matching_songs = function (req, resp){
     var input = req.params.search_key.toString();
     console.log(input);
     let i = 0;
-    let musicinfo = [];    
+    let musicinfo = [];
     tracks.forEach(element =>{
             if((element.track_title.toString().toLowerCase().includes(input.toString().toLowerCase())) 
             || (element.album_title.toString().toLowerCase().includes(input.toLowerCase())) || 
@@ -27,19 +57,47 @@ exports.search_songs = function (req, resp){
             }
         })
 
+        // Check if song is found
         if(musicinfo.length<1){
-            resp.status(404).send("Record not found");
+            resp.status(404).send("Song not present in Database");
         }
         else{
             resp.send(musicinfo);
         }
 };
 
-//to fetch all tracks
-exports.song_all = function (req, res, next) {
+// Retrieve the list of songs
+exports.get_all_songs = function (req, res, next) {
         try {
             res.status(200).send(tracks);
-        } catch (err) {
+        } catch (error_msg) {
             res.status(500).send('Error fetching matching track details!')
         }
+};
+
+// Return the sorted list of songs based on user reviews and rating
+exports.get_sorted_songs_limit_n = function (req, res, next) {
+    SongDB.find(
+        {hidden:{$ne:false}}
+        ).sort('-Rating').limit(10).populate({ 
+            path: 'Reviews', 
+            options: { sort: { _id: -1 }, limit: 2 },
+            populate: { path: 'user_id' } 
+            }).exec(function (error_msg, item) {
+                if (error_msg) return next(error_msg);
+                res.send(item);
+            });
+};
+
+// Retrieve the list of songs by id
+exports.get_songs_by_id = function (req, res, next) {
+    SongDB.findById(req.params.id).populate(
+            { 
+                path: 'Reviews', 
+                options: { sort: { _id: -1 }, limit: 2 }, 
+                populate: { path: 'user_id' } }
+            ).exec(function (error_msg, item) {
+                if (error_msg) return next(error_msg);
+                 res.send(item);
+            });
 };
